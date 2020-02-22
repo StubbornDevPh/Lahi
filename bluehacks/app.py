d569ca692622048d540dfcccab9f75b594360e70
Al Francis Gabriel Bolima <alfrancisgabriel@gmail.com>
2020-02-23 03:28:38 +0800

    weeee

--- bluehacks/app.py
+++ bluehacks/app.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template ,url_for ,request,redirect,session,logg
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session,sessionmaker
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 app.static_folder = 'static'
 app.TEMPLATES_AUTO_RELOAD=True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'anonymouseisreal'
+engine = create_engine('sqlite:///database.db')
+db=scoped_session(sessionmaker(bind=engine))
 
 db = SQLAlchemy(app)
 
@@ -48,14 +51,54 @@ def places():
 @app.route('/login',methods=['GET', 'POST'])
 def login():
     #code this
+    if request.method=="POST":
+
+        email=request.form['email']
+        password = request.form['password']
+        usernamedata=db.execute("SELECT email FROM accounts WHERE email=:email",{"email":email}).fetchone()
+        uresult = db.fetachall()
+        passworddata=db.execute("SELECT password FROM accounts WHERE email=:email",{"password":password}).fetchone()
+        presult = db.fetchall()
+
+        if usernamedata is None:
+            flash("No username","danger")
+            return render_template('login.html')
+        else:
+            for passwor_data in passworddata:
+                if sha256_crypt.verify(password,passwor_data):
+                    session["log"]=True
+                    flash("You are now logged in!!","success")
+                    return redirect(url_for('hello')) #to be edited from here do redict to either svm or home
+                else:
+                    flash("incorrect password","danger")
+                    return render_template('login.html')
     return render_template('login.html')
 
 @app.route('/register',methods=["POST","GET"])
 def register():
     if request.method == 'POST':
-        Account = Accounts(fname=request.form['fname'], email=request.form['email'], address=request.form['address'], contact=request.form['contact'], username=request.form['username'], password=request.form['password'])
-        db.session.add(Account)
-        db.session.commit()
+        fname=request.form['fname']
+        email=request.form['email']
+        address=request.form['address']
+        contact=request.form['contact']
+        username=request.form['username']
+        password=request.form['password']
+        secure_password=sha256_crypt.encrypt(str(password))
+        usernamedata=db.execute("SELECT email FROM accounts WHERE email=:email",{"email":email}).fetchone()
+    
+        if usernamedata==None:
+                if password==confirm:
+                    db.execute("INSERT INTO accounts(fname,email,address,contact,username,password) VALUES(:fname,:email,:address,:contact,:username,:password)",
+                        {"fname":fname,"email":email,"address":address,"contat":contact,'username':username,'password':secure_password})
+                    db.commit()
+                    flash("You are registered and can now login","success")
+                    return redirect(url_for('login'))
+                else:
+                    flash("password does not match","danger")
+                    return render_template('register.html')
+        else:
+            flash("user already existed, please login or contact admin","danger")
+            return redirect(url_for('login'))
     return render_template('register.html')
     
 if __name__ == "__main__":
