e78df65cb0fa74c330d32c3084ec1db6b45e814a
Al Francis Gabriel Bolima <alfrancisgabriel@gmail.com>
2020-02-23 00:16:25 +0800

    wew

--- bluehacks/createdb.py
+++ bluehacks/createdb.py
@@ -3,7 +3,7 @@ import sqlite3
 con = sqlite3.connect('database.db')
 print ("Opened database successfully")
 
-con.execute('CREATE TABLE accounts (id INTEGER NOT NULL,username TEXT,email TEXT, contact TEXT, pass TEXT, usertype TEXT,datecreate DATE)')
+con.execute('CREATE TABLE accounts (id INTEGER NOT NULL,fullname TEXT,username TEXT,email TEXT, contact TEXT, pass TEXT, usertype TEXT,datecreated DATE)')
 print ("TABLE CONNECTED SUCCESSFULLY")
 con.commit()
 con.close()
--- bluehacks/models.py
+++
@@ -1,3 +0,0 @@
-import sqlite3
-from os import path
-ROOT = path.dirname(path.relpath(__file__))
